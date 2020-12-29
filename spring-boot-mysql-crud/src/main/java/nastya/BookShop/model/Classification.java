3412e81356e37764ecc2249c703a2ff88ec65363
nastyalevk <levkovich.astya@gmail.com>
2020-12-29 13:00:15 +0300

    commit

--- spring-boot-mysql-crud/src/main/java/nastya/BookShop/model/Classification.java
+++ spring-boot-mysql-crud/src/main/java/nastya/BookShop/model/Classification.java
@@ -3,17 +3,20 @@ package nastya.BookShop.model;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.util.Set;
 
-
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "classif")
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
@@ -23,9 +26,9 @@ public class Classification {
     @Column(name = "classif_id")
     private Integer id;
 
-//    @OneToMany(mappedBy = "classification")
-//    @JsonIgnore
-//    private Set<Classification> classifications;
+    @OneToMany(mappedBy = "classification")
+    @JsonIgnore
+    private Set<Classification> classifications;
 
     @OneToMany(mappedBy = "classification")
     @JsonIgnore
@@ -39,9 +42,9 @@ public class Classification {
     @JsonIgnore
     private Set<Order> order;
 
-//    @ManyToOne
-//    @JoinColumn(name = "parent_id", nullable = false)
-//    private Classification classification;
+    @ManyToOne
+    @JoinColumn(name = "parent_id", nullable = false)
+    private Classification classification;
 
     private String name;
 
