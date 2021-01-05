ea8c7612fa629e91fc29e0ea113e6972d15dfd95
nastyalevk <levkovich.astya@gmail.com>
2021-01-05 18:27:05 +0300

    shop controller

--- spring-boot-mysql-crud/src/main/java/nastya/BookShop/controller/ShopController.java
+++ spring-boot-mysql-crud/src/main/java/nastya/BookShop/controller/ShopController.java
@@ -1,9 +1,18 @@
 package nastya.BookShop.controller;
 
+import nastya.BookShop.dto.shop.ShopDto;
 import nastya.BookShop.service.api.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -13,5 +22,20 @@ public class ShopController {
     @Autowired
     public ShopController(ShopService shopService) {
         this.shopService = shopService;
+    }
+
+    @PostMapping(path = "/shop-create")
+    public ResponseEntity createShop(@RequestBody ShopDto shopDto) {
+        shopService.saveShop(shopDto);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/shops/{id}")
+    public ResponseEntity findUserShops(@PathVariable("id") Integer id) {
+        List<ShopDto> shopDto = shopService.userShops(id);
+        if (shopDto.isEmpty()) {
+            return new ResponseEntity("No shops found", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity(shopDto, HttpStatus.OK);
     }
 }
